638129ae8bbb26470ffbc7710e4659cc69ff48ac
Kevin Quisquater <quisquater.kevin@gmail.com>
2017-10-19 23:33:29 +0200

    Fix: add a white background to the collection view

--- LBTAYouTubeTutorial/App flow/Home/HomeController.swift
+++ LBTAYouTubeTutorial/App flow/Home/HomeController.swift
@@ -17,6 +17,7 @@ class HomeController: UICollectionViewController {
     }
     
     private func setupViews() {
+        collectionView?.backgroundColor = .white
         setupNavigationBar()
     }
     
